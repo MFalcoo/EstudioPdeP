08637d9008f0a2fe76cbf11048a9645b4b1fc69b
MFalcoo <mfalco@frba.utn.edu.ar>
2022-06-08 22:18:38 -0300

    Terminado

--- Funciones/src/Lib.hs
+++ Funciones/src/Lib.hs
@@ -1,5 +1,8 @@
 module Lib where
 
+---------------Invertir parametros-----------------------
+--flip :: (a->b->c) -> b -> a -> c
+
 ---------------Resto-----------------------
 -- mod dividiendo divisor
 ejemplo = mod 3 2
@@ -69,3 +72,72 @@ ejemplo16 = reverse [1,2,3,4]
 ---------------Filtrar lista-----------------------
 --filter :: (a->Bool) -> [a] -> [a]
 ejemplo17 = filter (even) [1,2,3,4,5]
+
+---------------Transformar lista-----------------------
+--map ::  (a->b) -> [a] -> [b]
+ejemplo18 = map (+1) [1,2,3]
+
+---------------Todos en la lista cumplen-----------------------
+--all :: (a->Bool) -> [a] -> Bool
+ejemplo19 = all (==0) [0,0,0]
+
+---------------alguno en la lista cumple-----------------------
+--any :: (a->Bool) -> [a] -> Bool
+ejemplo20 = any (==0) [0,1,2]
+
+---------------Reducir/Plegar (Izq)-----------------------
+--foldl :: (a->b->a) -> a -> [b] -> a
+ejemplo21 = foldl (div) 16 [4,2]
+--λ> foldl (-) 20 [2,5,3]
+--10
+--λ> ((20-2)-5)-3
+--10
+--λ> foldl (-) 20 [2,5,3,4]
+--6
+--λ> (((20-2)-5)-3)-4
+--6
+
+---------------Reducir/Plegar (Der)-----------------------
+--foldr :: (b->a->a) -> a -> [b] -> a
+ejemplo22 = foldr (div) 4 [8,16]
+--λ> foldr (-) 20 [2,5,3]
+--(-20)
+--λ> 2-(5-(3-20))
+--(-20)
+--λ> foldr (-) 20 [2,5,3,4]
+--16
+--λ> 2-(5-(3-(4-20)))
+--16
+
+---------------Apareo con transformacion-----------------------
+--zipWith :: (a->b->c) -> [a] -> [b] -> [c]
+ejemplo23 = zipWith (,) ["Boca","River","Velez"] ["Godoy","Racing","Newels"]
+
+---------------Primer Elemento que cumple-----------------------
+--find :: (a->Bool) -> [a] -> a
+--ejemplo24 = find even [1,2,3,4,5,3]
+
+---------------Ordenar lista-----------------------
+--sort :: Ord a => [a] -> [a]
+--ejemplo25 = sort [2,3,4,51,1]
+
+
+----------------------------------------------------------
+----------------Generacion de Listas----------------------
+----------------------------------------------------------
+
+---------------Generar lista de 1 elemento dado infinitas veces -----------------------
+--repeat  :: a -> [a]
+ejemplo26 = repeat 'a'
+
+---------------Generar lista de un elemento que pasa x una funcion infinitas veces -----------------------
+--iterate :: (a->a) -> a -> [a]
+ejemplo27 = iterate (++"*") "Marco"
+
+---------------Generar lista de 1 elemento dado una cierta cantidad de veces-----------------------
+--replicate :: Int -> a -> [a]
+ejemplo28 = replicate 5 "hola"
+
+---------------Generar lista de 1 lista dada infinitas veces-----------------------
+--cycle  :: [a] -> [a]
+ejemplo29 = cycle [1,2,3,4]
